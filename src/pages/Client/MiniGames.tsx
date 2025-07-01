fde6c0fdfb8ae0a2564b6158c51249d448a3dbe5
N-Dave-08 <cdave.dev@gmail.com>
2025-07-01 20:41:06 +0800

    feat: add Flappy Bird game component and background image to sidebar

--- src/pages/Client/MiniGames.tsx
+++ src/pages/Client/MiniGames.tsx
@@ -1,5 +1,11 @@
+import FlappyBird from "@/features/flappy-bird";
 import ClientLayout from "@/layouts/ClientLayout";
 
 export default function MiniGames() {
-	return <ClientLayout>MiniGames</ClientLayout>;
+	return (
+		<ClientLayout>
+			<p>flappy bird</p>
+			<FlappyBird />
+		</ClientLayout>
+	);
 }
