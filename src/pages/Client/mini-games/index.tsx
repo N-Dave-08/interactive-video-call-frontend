a2e3c169ffbff1295d55a427a7700e00c5e09640
N-Dave-08 <cdave.dev@gmail.com>
2025-07-04 03:27:25 +0800

    refactor: simplify animation logic and improve card layout in mini-games page

--- src/pages/Client/mini-games/index.tsx
+++ src/pages/Client/mini-games/index.tsx
@@ -16,48 +16,44 @@ export default function MiniGamesPage() {
 
 	return (
 		<div className="grid grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-4">
-			{miniGames.map((item, index) => {
+			{miniGames.map((item) => {
 				const Icon = item.icon;
 				const slug = item.name.toLowerCase().replace(/\s+/g, "-");
 
 				return (
 					<MotionCard
 						whileHover="hover"
-						initial={{ opacity: 0, y: 50, scale: 0.8 }}
-						animate={{
-							opacity: 1,
-							y: 0,
-							scale: 1,
-							transition: {
-								delay: index * 0.2,
-								duration: 0.4,
-								ease: "easeOut",
-							},
-						}}
+						initial="initial"
+						animate="initial"
 						className="hover:cursor-pointer relative overflow-hidden"
 						key={item.name}
 						onClick={() => navigate(`/client/mini-games/${slug}`)}
 					>
 						<motion.div
-							initial="initial"
-							animate="initial"
-							whileHover="hover"
 							variants={{
-								initial: { opacity: 0, y: 10 },
-								hover: { opacity: 1, y: 0 },
+								initial: { opacity: 0 },
+								hover: { opacity: 1 },
 							}}
-							className="flex items-center justify-center h-34 z-50 font-extrabold text-2xl text-gray- text-shadow-lg"
-						>
-							Click to Play
-						</motion.div>
+							className="absolute inset-0 bg-black/40 z-40"
+						/>
 						<motion.div
 							variants={{
 								inital: { scale: 1 },
 								hover: { scale: 1.1 },
 							}}
-							className="size-full absolute inset-0 bg-[url(/flappy-bird-01.jpg)] bg-cover opacity-50"
+							className="size-full absolute inset-0 bg-[url(/flappy-bird-01.jpg)] bg-cover z-20"
 						/>
-						<CardFooter className="justify-between z-50">
+						<motion.div
+							variants={{
+								initial: { opacity: 0, y: 10 },
+								hover: { opacity: 1, y: 0 },
+							}}
+							className="flex items-center justify-center h-34 font-extrabold text-2xl text-secondary text-shadow-lg z-50"
+						>
+							Click to Play
+						</motion.div>
+
+						<CardFooter className="justify-between z-50 text-secondary">
 							<p className="font-bold text-gray- text-shadow-lg">{item.name}</p>
 							<div className="rounded-full bg-secondary/40 p-2">
 								<Icon className="size-5" />
