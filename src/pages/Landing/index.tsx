0cd9b919f47878dc6b544ecc03c55d36c4b3ccde
N-Dave-08 <cdave.dev@gmail.com>
2025-08-01 22:55:51 +0800

    chore: clean up

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -19,7 +19,6 @@ export default function LandingPage() {
 
 	return (
 		<div className="flex flex-col min-h-screen">
-			ASDASDASDASDSAD
 			<BlurBackground />
 			<Navbar />
 			{sections.map((Section, idx) => (
