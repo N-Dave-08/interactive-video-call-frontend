e4af589e2296f3604e4c423ee15d4c91be9719b9
N-Dave-08 <cdave.dev@gmail.com>
2025-08-07 08:59:47 +0800

    feat(sidebar): add background image and gradient overlay to AppSidebar

--- src/layouts/sidebars/AppSidebar.tsx
+++ src/layouts/sidebars/AppSidebar.tsx
@@ -77,12 +77,14 @@ export default function AppSidebar() {
 
 	const items = user?.role === "admin" ? adminItems : clientItems;
 
+	const bgUrl = "forest.jpg";
+
 	return (
 		<Sidebar>
-			{/* <div
-			// className="absolute inset-0 bg-cover bg-center opacity-60 pointer-events-none bg-primary"
-			// style={{ backgroundImage: `url(/backgrounds${bgUrl})` }}
-			/> */}
+			<div
+				className="absolute inset-0 bg-cover bg-center opacity-60 pointer-events-none"
+				style={{ backgroundImage: `url(/backgrounds/${bgUrl})` }}
+			/>
 			<SidebarHeader>
 				<SidebarMenu>
 					<SidebarMenuItem>
@@ -103,7 +105,7 @@ export default function AppSidebar() {
 
 			<SidebarContent className="">
 				{/* gradient */}
-				{/* <div className="absolute inset-0 bg-gradient-to-b from-white/80 via-white/60 to-blue-100/60 pointer-events-none" /> */}
+				<div className="absolute inset-0 bg-gradient-to-b from-white/80 via-white/60 to-blue-100/60 pointer-events-none" />
 				{/* Menu Tabs */}
 				<SidebarGroup>
 					<SidebarGroupLabel>Application</SidebarGroupLabel>
